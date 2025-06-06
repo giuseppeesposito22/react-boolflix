bb948fab83e56929c0ec6fa2f953734fb3940d7d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-06 14:53:10 +0200

    add production card component

---
+++ src/components/productions/ProductionCard.jsx
@@ -0,0 +1,49 @@
+export default function ProductionCard({ production }) {
+  // Gestione bandiere per le lignue
+  const language = {
+    en: "gb",
+    ja: "jp",
+    ko: "kr",
+  };
+
+  const flagByLanguage = (lang) => {
+    const flagLanguage = language[lang] ?? lang;
+
+    return `https://flagcdn.com/w20/${flagLanguage}.png`;
+  };
+
+  // Generazione delle stelle per il rating
+  const generateRate = (vote) => {
+    let ratingStar = [];
+
+    const rating = Math.floor(vote) / 2;
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        ratingStar.push(<i key={i} className="bi bi-star-fill"></i>);
+      } else {
+        ratingStar.push(<i key={i} className="bi bi-star"></i>);
+      }
+    }
+
+    return ratingStar;
+  };
+
+  return (
+    <ul key={production.id}>
+      <li>
+        {
+          <img
+            src={`https://image.tmdb.org/t/p/w92${production.poster_path}`}
+            alt=""
+          />
+        }
+        Titolo: {production.title || production.name}, Titolo originale:
+        {production.original_title || production.original_name}, Lingua:
+        {<img src={flagByLanguage(production.original_language)} alt="" />},
+        Voto:
+        {generateRate(production.vote_average)}
+      </li>
+    </ul>
+  );
+}
--- src/components/productions/ProductionsLIst.jsx
+++ src/components/productions/ProductionsLIst.jsx
@@ -1,56 +1,13 @@
 import { useProduction } from "../../contexts/ProductionContext";
+import ProductionCard from "./ProductionCard";
 
 export default function ProductionsList() {
   const { productions } = useProduction();
 
-  // Gestione bandiere per le lignue
-  const language = {
-    en: "gb",
-    ja: "jp",
-    ko: "kr",
-  };
-
-  const flagByLanguage = (lang) => {
-    const flagLanguage = language[lang] ?? lang;
-
-    return `https://flagcdn.com/w20/${flagLanguage}.png`;
-  };
-
-  // Generazione delle stelle per il rating
-  const generateRate = (vote) => {
-    let ratingStar = [];
-
-    const rating = Math.floor(vote) / 2;
-
-    for (let i = 1; i <= 5; i++) {
-      if (i <= rating) {
-        ratingStar.push(<i key={i} className="bi bi-star-fill"></i>);
-      } else {
-        ratingStar.push(<i key={i} className="bi bi-star"></i>);
-      }
-    }
-
-    return ratingStar;
-  };
-
   return (
     <>
       {productions.map((production) => (
-        <ul key={production.id}>
-          <li>
-            {
-              <img
-                src={`https://image.tmdb.org/t/p/w92${production.poster_path}`}
-                alt=""
-              />
-            }
-            Titolo: {production.title || production.name}, Titolo originale:
-            {production.original_title || production.original_name}, Lingua:
-            {<img src={flagByLanguage(production.original_language)} alt="" />},
-            Voto:
-            {generateRate(production.vote_average)}
-          </li>
-        </ul>
+        <ProductionCard key={production.id} production={production} />
       ))}
     </>
   );
